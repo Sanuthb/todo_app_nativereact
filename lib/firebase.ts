98a31496b381760c1d557e3bbe23e07f4cf253ee
Sanuth Bibin.B <165442353+Sanuthb@users.noreply.github.com>
2026-02-24 13:12:22 +0530

    completed

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { Auth, getAuth, getReactNativePersistence, initializeAuth } from 'fireba
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB6ICqxOMNnSUiqXLYY92YGZtWLRK7vWGM",
-  authDomain: "todoapp-45f45.firebaseapp.com",
-  projectId: "todoapp-45f45",
-  storageBucket: "todoapp-45f45.firebasestorage.app",
-  messagingSenderId: "266285049783",
-  appId: "1:266285049783:web:7875db1ad6657e8adbaa32",
-  measurementId: "G-B2LNC5FDH0"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY ||"AIzaSyB6ICqxOMNnSUiqXLYY92YGZtWLRK7vWGM",
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN ||"todoapp-45f45.firebaseapp.com",
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID ||"todoapp-45f45",
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET ||"todoapp-45f45.firebasestorage.app",
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ||"266285049783",
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID ||"1:266285049783:web:7875db1ad6657e8adbaa32",
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID ||"G-B2LNC5FDH0"
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
